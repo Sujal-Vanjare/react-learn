96657d5ed0060d2eb21e392db44fcfaa5a955832
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2025-03-20 14:55:58 +0530

    more ways of conditional rendering

--- src/app/conditional-rendering/page.jsx
+++ src/app/conditional-rendering/page.jsx
@@ -1,3 +1,5 @@
+import { fchown } from "fs";
+
 const products = [
   { title: "Cabbage", isFruit: false, id: 1 },
   { title: "Garlic", isFruit: false, id: 2 },
@@ -14,6 +16,37 @@ export default function ShoppingList() {
     >
       {product.title}
     </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+}
+
+function ShowContent() {
+  let content;
+  if (isLoggedIn) {
+    content = <AdminPanel />;
+  } else {
+    content = <LoginForm />;
+  }
+  return <div>{content}</div>;
+}
+
+function ShowContentSec() {
+  return <div>{isLoggedIn ? <AdminPanel /> : <LoginForm />}</div>;
+}
+
+function ShowContentThree() {
+  return <div>{isLoggedIn && <AdminPanel />}</div>;
+}
+
+function RenderingList() {
+  const products = [
+    { title: "Cabbage", id: 1 },
+    { title: "Garlic", id: 2 },
+    { title: "Apple", id: 3 },
+  ];
+  const listItems = products.map((product) => (
+    <li key={product.id}>{product.title}</li>
   ));
 
   return <ul>{listItems}</ul>;
